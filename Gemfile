a54d7e1bdf14d144d42b045264357305a5ede526
Almazzzzz <badrtdinov_a@mail.ru>
2016-06-29 17:06:12 +0300

    feat: Add capybara and authentication tests

--- Gemfile
+++ Gemfile
@@ -45,9 +45,10 @@ gem 'omniauth-facebook'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
-  gem 'rspec-rails'
+  gem 'rspec-rails', '~> 3.4'
   gem 'factory_girl_rails'
   gem 'ffaker'
+  gem 'capybara'
 end
 
 group :development do
@@ -56,9 +57,9 @@ group :development do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
-
+end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-end
+
 
 
---
+++ spec/features/auth_spec.rb
@@ -0,0 +1,128 @@
+require 'rails_helper'
+
+RSpec.describe "Auth", :type => :feature do
+  
+  it "should create new user account" do
+    visit new_user_registration_path
+    fill_in "Email", :with => "q@q.ru"
+    fill_in "Password", :with => "qwerty"
+    fill_in "Password confirmation", :with => "qwerty"
+    click_button 'Sign up'
+    
+    expect(page).to have_content('Welcome! You have signed up successfully.')
+  end
+  
+  it "should successfully login" do
+    user = FactoryGirl.create(:user, :email => "w@w.ru", :password => "qwerty", 
+                              :password_confirmation => "qwerty")
+    visit new_user_session_path
+    fill_in "Email", :with => "w@w.ru"
+    fill_in "Password", :with => "qwerty"
+    click_button 'Log in'
+    
+    expect(page).to have_content('Signed in successfully.')
+  end
+
+  it "should retry to input correct email/password" do
+    user = FactoryGirl.create(:user, :email => "w@w.ru", :password => "qwerty", 
+                              :password_confirmation => "qwerty")
+    visit new_user_session_path
+    fill_in "Email", :with => "q@q.ru"
+    fill_in "Password", :with => "qwerty123"
+    click_button 'Log in'
+    
+    expect(page).to have_content('Invalid Email or password.')
+  end
+
+  it "should successfully sign out" do
+    user = FactoryGirl.create(:user, :email => "w@w.ru", :password => "qwerty", 
+                              :password_confirmation => "qwerty")
+    visit new_user_session_path
+    fill_in "Email", :with => "w@w.ru"
+    fill_in "Password", :with => "qwerty"
+    click_button 'Log in'
+    click_link "Sign Out"
+    
+    expect(page).to have_content('Signed out successfully.')
+  end
+
+  it "should successfully change account data" do
+    user = FactoryGirl.create(:user, :email => "w@w.ru", :password => "qwerty", 
+                              :password_confirmation => "qwerty")
+    visit new_user_session_path
+    fill_in "Email", :with => "w@w.ru"
+    fill_in "Password", :with => "qwerty"
+    click_button 'Log in'
+    visit edit_user_registration_path
+    fill_in "Email", :with => "w2@w2.ru"
+    fill_in "Password", :with => "qwerty123"
+    fill_in "Password confirmation", :with => "qwerty123"
+    fill_in "Current password", :with => "qwerty"
+    click_button 'Update'
+    
+    expect(page).to have_content('Your account has been updated successfully.')
+  end
+
+  it "should not restore password w/a conf email when logged in" do
+    user = FactoryGirl.create(:user, :email => "w@w.ru", :password => "qwerty", 
+                              :password_confirmation => "qwerty")
+    visit new_user_session_path
+    fill_in "Email", :with => "w@w.ru"
+    fill_in "Password", :with => "qwerty"
+    click_button 'Log in'
+    visit edit_user_password_path
+    
+    expect(page).to have_content('You are already signed in.')
+  end
+
+  it "should not restore password w/a conf email when not logged in" do
+    visit edit_user_password_path
+    
+    expect(page).to have_content("You can't access this page without coming from a password reset email.")
+  end
+
+  it "should send email to restore password" do
+    user = FactoryGirl.create(:user, :email => "w@w.ru", :password => "qwerty", 
+                              :password_confirmation => "qwerty")
+    visit new_user_password_path
+    fill_in 'Email', :with => "w@w.ru"
+    click_button 'Send me reset password instructions'
+    
+    expect(page).to have_content('You will receive an email with instructions')
+  end
+
+  it "should 'remember me' after login" do
+    user = FactoryGirl.create(:user, :email => "w@w.ru", :password => "qwerty", 
+                              :password_confirmation => "qwerty", :remember_created_at => nil)
+    visit new_user_session_path
+    fill_in "Email", :with => "w@w.ru"
+    fill_in "Password", :with => "qwerty"
+    page.check('Remember me')
+    click_button 'Log in'
+    expect(user.remember_created_at).to eq(DateTime.now)
+    #expect(user.remember_me).to eq(1)
+    #expect(page).to have_content('Signed in successfully.')
+  end
+
+    #it "should successfully delete account", :js => true do
+    it "should successfully delete account" do
+    user = FactoryGirl.create(:user, :email => "w@w.ru", :password => "qwerty", 
+                              :password_confirmation => "qwerty")
+    visit new_user_session_path
+    fill_in "Email", :with => "w@w.ru"
+    fill_in "Password", :with => "qwerty"
+    click_button 'Log in'
+    visit edit_user_registration_path
+    click_button 'Cancel my account'
+    #page.accept_confirm { click_button 'Cancel my account' }
+    #page.driver.browser.switch_to.alert.dismiss
+    #page.evaluate_script('window.confirm = function() { return true; }')
+    expect(page).to have_content('Bye! Your account has been successfully cancelled.')
+  end
+end
+
+
+# сгенерировать rails generate rspec:controller user и посмотреть, что получится. 
+# попробовать прогнать эти тесты 
+# отдельно посмотреть интеграцию rspec и devise
+# интеграция rspec и omniauth
