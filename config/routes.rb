55b1e68e617a5cae41b45ff9adbec217b7999e9a
Alexey Poimtsev <alec@alec-c4.com>
2016-06-30 22:06:31 +0300

    feat: add show profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'profile/show'
+
+  get 'profile/edit'
+
   resources :flats
   resources :posts
   root 'welcome#index'
---
+++ spec/factories/profiles.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :profile do
+    first_name "MyString"
+    last_name "MyString"
+    middle_name "MyString"
+    phone "MyString"
+    avatar "MyString"
+    user nil
+  end
+end
